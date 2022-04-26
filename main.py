9e69f79d2e0f39324531b0f2f44649a5f564557b
Alexander Marjin <amarjin6@gmail.com>
2022-04-27 01:20:52 +0300

    Add object tracker

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from objectTracker import *
 # Load Object Detection
 od = ObjectDetection()
 
+# Load Object Tracker
+ot = MahalanobisDistTracker()
+
 # Objects Allowed
 allowed_objects = ['person', 'dog', 'cat']
+
+# Objects Detected
+detected_objects = []
 
 # Load the video stream
 cap = cv2.VideoCapture('cam\\elevator1080p.mp4')
@@ -22,11 +28,15 @@ while True:
         class_name = od.classes[class_id]
 
         if class_name in allowed_objects:
+            detected_objects.append(box)
+            # Track objects
+            bbs = ot.update(detected_objects)
             color = od.colors[class_id]
-            cv2.putText(img, f'{class_name}', (x, y - 15), 0, 1, color, 1)
+            cv2.putText(img, f'{class_name}: {bbs[-1][-1]}', (x, y - 10), 0, 0.5, color, 1)
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
 
-    cv2.imshow('I', img)
+    cv2.imshow('Cam', img)
+
     key = cv2.waitKey(1)
     if key == 27:
         break
